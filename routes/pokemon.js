001889b71712a7186b92bbeafc3b72b1642e6e64
mfahriferdiansyah <xkadzuvic@gmail.com>
2023-03-09 07:04:45 +0700

    refactor: Add /pub/ to endpoint

--- routes/pokemon.js
+++ routes/pokemon.js
@@ -3,7 +3,7 @@ var router = express.Router();
 const authentication = require('../middlewares/authentication');
 const PokemonController = require('../controllers/pokemonController');
 
-router.get('/pokemons', authentication, PokemonController.getPokemon);
-router.post('/pokemons', authentication, PokemonController.gachaPokemon);
+router.get('/pub/pokemons', authentication, PokemonController.getPokemon);
+router.post('/pub/pokemons', authentication, PokemonController.gachaPokemon);
 
 module.exports = router
--- routes/user.js
+++ routes/user.js
@@ -2,7 +2,7 @@ var express = require('express');
 const UserController = require('../controllers/userController');
 var router = express.Router();
 
-router.post('/login', UserController.login);
-router.post('/register', UserController.register);
+router.post('/pub/login', UserController.login);
+router.post('/pub/register', UserController.register);
  
 module.exports = router
